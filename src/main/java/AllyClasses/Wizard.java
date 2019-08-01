a0d2eae490af8f7ddea9c67288fd8a0be1dd71df
Jeremy Wood <jeremy.wood.nz@gmail.com>
2019-08-01 13:02:09 +0100

    added Wizard and Wand class

--- src/main/java/AllyClasses/Wizard.java
+++ src/main/java/AllyClasses/Wizard.java
@@ -1,8 +1,23 @@
 package AllyClasses;
 
-public class Wizard {
+import Items.ArmourType;
+import Items.Wand;
+import Items.WandType;
 
+public class Wizard extends Ally{
 
+    private Wand wand;
 
+    public Wizard(String name) {
+        super(ArmourType.CLOTH,75,name);
+        this.wand = new Wand(WandType.BAD);
+    }
 
+    public Wand getWand() {
+        return wand;
+    }
+
+    public void setWand(Wand wand) {
+        this.wand = wand;
+    }
 }
